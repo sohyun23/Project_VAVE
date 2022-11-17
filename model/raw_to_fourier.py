67c3864614a8a1cab90522967f1184928a90e0fe
Chaewon <cw001121@naver.com>
2022-11-17 15:53:47 +0900

    seperate raw_to_fourier function to fs_to_freq

--- model/raw_to_fourier.py
+++ model/raw_to_fourier.py
@@ -1,28 +1,30 @@
 import numpy as np
 
-def raw_to_fourier(df, fs):
-    
+
+def fs_to_freq(n, fs):
+
+    k = np.arange(n)
+    Fs = fs
+    T = n/Fs
+    freq = k/T
+    freq = freq[range(int(n))]
+
+    return freq
+
+
+def raw_to_fourier(df):
+
     fft_lst = []
 
     iter = len(df.columns)
     n = len(df)
 
-    k = np.arange(n) 
-    Fs = fs
-    T = n/Fs 
-    freq = k/T
-    freq = freq[range(int(n))]
-
     for i in range(iter):
-        
+
         df_col = df.iloc[:, [i]]
-
         Y = np.fft.fft(df_col)/n
-        
         Y = Y[range(int(n/2))]
-
-        abs_Y = abs(Y) ** 2 
-
+        abs_Y = abs(Y) ** 2
         fft_lst.append(abs_Y)
 
-    return fft_lst, freq
+    return fft_lst
