abebff0b5e2f81f21147647aa01753064b3ff555
izongg0 <108528803+izongg0@users.noreply.github.com>
2022-11-30 02:15:52 +0900

    Web construction (#54)
    * Feature/#30 build visualization module (#45)
    
    * add raw_to_fourier
    
    * Feature/#3 build fft function (#44)
    
    * docs : TIL
    
    * Add: fft func
    
    * Delete workspace directory
    
    * add: generate fft function code
    
    * Add: generate fft function
    
    * add import
    
    * add import
    
    * add import
    
    * add python argument parser
    
    * seperate raw_to_fourier function to fs_to_freq
    
    * fix fs_to_freq
    
    * fix merge function to merge ndarray to DataFrame
    
    * make 10000 features visualization module
    
    * build visualization graph
    
    Co-authored-by: sohyun23 <108653282+sohyun23@users.noreply.github.com>
    
    * Revert "Feature/#30 build visualization module (#45)" (#46)
    
    This reverts commit 71eb864144d01b1cd0a9a0673a5fff9c205e4688.
    
    * Build construction of web
    
    * Build Web Frame
    
    Co-authored-by: 이채원 <94580501+cw001121@users.noreply.github.com>
    Co-authored-by: sohyun23 <108653282+sohyun23@users.noreply.github.com>
    Co-authored-by: qweasdfdsa11 <108528803+qweasdfdsa11@users.noreply.github.com>

---
+++ vave/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Frame />
+  </div>
+</template>
+
+<script>
+import Frame from '../components/Frame.vue'
+
+export default {
+  conponents: {
+    Frame: Frame
+  },
+  data() {
+    return {}
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
