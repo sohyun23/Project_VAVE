c8a4d4363a6b286c33848c81547395ca32ff4219
sohyun23 <sh.cho.020@gmail.com>
2022-12-11 14:41:47 +0900

    generate pickle

--- model/raw_to_fourier.py
+++ model/raw_to_fourier.py
@@ -1,37 +1,7 @@
 import numpy as np
 
-<<<<<<< HEAD
 
 def fs_to_freq(n, fs):
-
-    k = np.arange(n)
-    Fs = int(fs)
-    T = n/Fs
-    freq = k/T
-    freq = freq[range(int(n/2))]
-
-    return freq
-
-
-def raw_to_fourier(df):
-
-    fft_lst = []
-=======
->>>>>>> f514b382ca4787aed938743803cc3f94e622bae0
-
-def fs_to_freq(n, fs):
-
-<<<<<<< HEAD
-    for i in range(iter):
-
-        df_col = df.iloc[:, [i]]
-        Y = np.fft.fft(df_col)/n
-        Y = Y[range(int(n/2))]
-        abs_Y = abs(Y) ** 2
-        fft_lst.append(abs_Y)
-
-    return fft_lst
-=======
     k = np.arange(n)
     Fs = int(fs)
     T = n/Fs
@@ -60,4 +30,3 @@ def raw_to_fourier(wav_np):
     #     fft_lst.append(abs_Y)
 
     return signal
->>>>>>> f514b382ca4787aed938743803cc3f94e622bae0
